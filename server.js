101ab97dd7fe5795795faf2ea2e2f7c18dd87e4d
joe <joseph.beuckman@usbank.com>
2017-04-13 10:35:43 -0500

    building a nodejs stopgap to repeat stdout from kinect to the command line

---
+++ server.js
@@ -0,0 +1,45 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+// Broadcast to all.
+wss.broadcast = function broadcast(data) {
+  wss.clients.forEach(function each(client) {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(data);
+    }
+  });
+};
+
+/*
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(data) {
+    // Broadcast to everyone else.
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(data);
+      }
+    });
+  });
+});
+*/
+
+
+const spawn = require('child_process').spawn;
+
+var options = {
+    cwd: '/Users/innovator/Kinect/NITE-Bin-Dev-MacOSX-v1.5.2.21/Samples/Bin/x64-Release'
+};
+
+
+const bat = spawn('./GestureRepeater', [], options);
+
+const rl = require('readline').createInterface({
+  input: bat.stdout
+});
+
+rl.on('line', (line) => {
+  console.log(line);
+    wss.broadcast(line);
+});
+
